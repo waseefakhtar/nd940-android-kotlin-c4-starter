0cb5439412e2478fb74cf955f8526f51e131f421
Waseef Akhtar <waseef@degoo.com>
2020-03-19 02:18:45 +0500

    Implementing AuthenticationActivity

--- final/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ final/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,10 +1,14 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
 
 
 /**
@@ -19,18 +23,47 @@ class AuthenticationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
 //      TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
-        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), RC_SIGN_IN)
-
 //      TODO: If the user was authenticated, send him to RemindersActivity
+
         val auth: FirebaseAuth = FirebaseAuth.getInstance()
         if (auth.currentUser != null) {
             // already signed in
+            startActivity(Intent(this, RemindersActivity::class.java))
+
         } else {
-            // not signed in
+            // Choose authentication providers
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build())
+
+            // Create and launch sign-in intent
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN)
         }
 
 //      TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                startActivity(Intent(this, RemindersActivity::class.java))
+
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 }
--- final/app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ final/app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -1,15 +1,20 @@
 package com.udacity.project4.locationreminders
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
+import com.firebase.ui.auth.AuthUI
 import com.udacity.project4.R
+import com.udacity.project4.authentication.AuthenticationActivity
 import kotlinx.android.synthetic.main.activity_reminders.*
+import java.util.logging.Logger
 
 /**
  * The RemindersActivity that holds the reminders fragments
@@ -19,13 +24,21 @@ class RemindersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reminders)
-
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
                 (nav_host_fragment as NavHostFragment).navController.popBackStack()
+                return true
+            }
+            R.id.logout -> {
+                AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener {
+                        startActivity(Intent(this, AuthenticationActivity::class.java))
+
+                    }
                 return true
             }
         }
