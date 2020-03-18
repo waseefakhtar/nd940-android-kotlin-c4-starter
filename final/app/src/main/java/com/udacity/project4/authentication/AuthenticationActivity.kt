23d65a7f589b16934a8f4b18711a3e38370c4416
Waseef Akhtar <waseef@degoo.com>
2020-03-19 01:00:53 +0500

    Adding FirebaseUI-Auth and calling createSignInIntentBuilder

--- final/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ final/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -2,22 +2,34 @@ package com.udacity.project4.authentication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
  * signed in users to the RemindersActivity.
  */
+private const val RC_SIGN_IN: Int = 123
+
 class AuthenticationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+//      TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), RC_SIGN_IN)
 
-//          TODO: If the user was authenticated, send him to RemindersActivity
+//      TODO: If the user was authenticated, send him to RemindersActivity
+        val auth: FirebaseAuth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            // already signed in
+        } else {
+            // not signed in
+        }
 
-//          TODO: a bonus is to customize the sign in flow to look nice using :
+//      TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
     }
