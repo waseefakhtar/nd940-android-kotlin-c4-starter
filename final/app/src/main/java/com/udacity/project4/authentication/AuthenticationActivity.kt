e7475b42d54ca14c00a1d40be0c016658b6d7d42
Waseef Akhtar <waseef@degoo.com>
2020-03-20 00:21:15 +0500

    Adding a custom auth layout

--- final/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ final/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -3,9 +3,13 @@ package com.udacity.project4.authentication
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.common.logging.Logger
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
 import com.udacity.project4.locationreminders.RemindersActivity
@@ -41,12 +45,29 @@ class AuthenticationActivity : AppCompatActivity() {
                 AuthUI.getInstance()
                     .createSignInIntentBuilder()
                     .setAvailableProviders(providers)
+                    .setTheme(R.style.CustomTheme)
+                    .setAuthMethodPickerLayout(setCustomAuthLayout())
                     .build(),
                 RC_SIGN_IN)
         }
 
 //      TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+    }
+
+    private fun setCustomAuthLayout(): AuthMethodPickerLayout {
+        val customLayout = AuthMethodPickerLayout
+            .Builder(R.layout.layout_authentication)
+            .setGoogleButtonId(R.id.google_sign_in_button)
+            .setEmailButtonId(R.id.email_sign_in_button)
+            .build()
+
+        val firebaseApp = FirebaseApp.getInstance()
+        AuthUI.getInstance(firebaseApp).createSignInIntentBuilder()
+            .setAuthMethodPickerLayout(customLayout)
+            .build()
+
+        return customLayout
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
